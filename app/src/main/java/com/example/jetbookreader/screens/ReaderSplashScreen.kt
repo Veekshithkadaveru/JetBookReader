5d580c1621b87b88e935478507d30c47f19dce7e
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-09-05 18:18:21 +0530

    Create Splash Screen

--- app/src/main/java/com/example/jetbookreader/screens/ReaderSplashScreen.kt
+++ app/src/main/java/com/example/jetbookreader/screens/ReaderSplashScreen.kt
@@ -1,10 +1,59 @@
 package com.example.jetbookreader.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun ReaderSplashScreen(navController: NavController) {
-    Text(text = "Splash Screen")
+    Surface(
+        modifier = Modifier
+            .padding(15.dp)
+            .size(330.dp),
+        shape = CircleShape,
+        border = BorderStroke(
+            width = 2.dp,
+            color = Color.LightGray
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(1.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Jet Book Reader",
+                style = MaterialTheme.typography.displaySmall,
+                color = Color.Red.copy(alpha = 0.5f)
+            )
+            Spacer(modifier = Modifier.height(15.dp))
+            Text(
+                text = "\"Read. Change. Yourself\"",
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.LightGray
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun ReaderSplashScreenPreview() {
+    ReaderSplashScreen(navController = rememberNavController())
 }
