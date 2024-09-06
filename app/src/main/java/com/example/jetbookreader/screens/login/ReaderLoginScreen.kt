7303a1f334b9d8aeb61f357fffda6c49f7eab194
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-09-06 22:45:01 +0530

    Setup User Form

--- app/src/main/java/com/example/jetbookreader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/example/jetbookreader/screens/login/ReaderLoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.jetbookreader.screens.login
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -17,10 +18,12 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.jetbookreader.components.EmailInput
+import com.example.jetbookreader.components.PasswordInput
 import com.example.jetbookreader.components.ReaderLogo
 
 @Composable
@@ -31,17 +34,24 @@ fun ReaderLoginScreen(navController: NavController) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             ReaderLogo()
-            UserForm()
+            UserForm(loading = false, isCreateAccount = true) { email, password ->
+                Log.d("Form", "ReaderLogin:$email $password")
+
+            }
         }
     }
 }
 
 @Composable
-fun UserForm() {
+fun UserForm(
+    loading: Boolean = false,
+    isCreateAccount: Boolean = false,
+    onDone: (String, String) -> Unit = { email, pwd -> }
+) {
     val email = rememberSaveable { mutableStateOf("") }
     val password = rememberSaveable { mutableStateOf("") }
     val passwordVisibility = rememberSaveable { mutableStateOf(false) }
-    val passwordFocusRequest = FocusRequester.Default
+    val passwordFocusRequest = remember { FocusRequester() }
     val keyboardController = LocalSoftwareKeyboardController.current
     val valid = remember(email.value, password.value) {
         email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
@@ -51,9 +61,20 @@ fun UserForm() {
         .background(MaterialTheme.colorScheme.background)
         .verticalScroll(rememberScrollState())
     Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        EmailInput(emailState = email, enabled = true, onAction = KeyboardActions {
+        EmailInput(emailState = email, enabled = !loading, onAction = KeyboardActions {
             passwordFocusRequest.requestFocus()
         })
+        PasswordInput(modifier = Modifier.focusRequester(passwordFocusRequest),
+            passwordState = password,
+            labelId = "Password",
+            enabled = !loading,
+            passwordVisibility = passwordVisibility,
+            onAction = KeyboardActions {
+                if (!valid) return@KeyboardActions
+                onDone(email.value.trim(), password.value.trim())
+            })
     }
 }
+
+
 
