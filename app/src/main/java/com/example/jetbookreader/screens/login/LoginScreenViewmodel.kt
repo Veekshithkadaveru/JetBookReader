f3bc4e8f16596a7c42c0375b6d5f7e1e5ee8f450
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-09-20 19:11:35 +0530

    Create Login Screen ViewModel

---
+++ app/src/main/java/com/example/jetbookreader/screens/login/LoginScreenViewmodel.kt
@@ -0,0 +1,40 @@
+package com.example.jetbookreader.screens.login
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+class LoginScreenViewmodel : ViewModel() {
+    private val auth: FirebaseAuth = Firebase.auth
+
+    private val _loading = MutableStateFlow(false)
+    val loading: MutableStateFlow<Boolean> = _loading
+
+    fun signInWithEmailAndPassword(email: String, password: String)=
+        viewModelScope.launch{
+        try {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        TODO("Take User to Home")
+                    } else {
+                        Log.d("FB", "signInWithEmailAndPassword: ${task.result}")
+                    }
+
+                }
+
+        } catch (ex: Exception) {
+            Log.d("FB", "signInWithEmailAndPassword:${ex.message}")
+        }
+
+    }
+
+    fun createUserWithEmailAndPassword() {
+
+    }
+}
