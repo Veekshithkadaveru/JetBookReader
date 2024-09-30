95e763f0b557eed799e2c0cdf2de57f602cc0b2e
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-09-30 19:54:10 +0530

    Setup Retrofit

---
+++ app/src/main/java/com/example/jetbookreader/network/BooksApi.kt
@@ -0,0 +1,19 @@
+package com.example.jetbookreader.network
+
+import com.example.jetbookreader.model.Book
+import com.example.jetbookreader.model.Item
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import javax.inject.Singleton
+
+@Singleton
+interface BooksApi {
+
+    @GET("volumes")
+    suspend fun getAllBooks(@Query("q") query: String): Book
+
+    @GET("volumes/{bookId}")
+    suspend fun getBookInfo(@Path("bookId") bookId: String): Item
+
+}
