9d98bdedcea5180857b6775e2edc7ebcc6fc5fc9
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-10-04 18:49:33 +0530

    Add Book Description

--- app/src/main/java/com/example/jetbookreader/screens/details/BookDetailsScreen.kt
+++ app/src/main/java/com/example/jetbookreader/screens/details/BookDetailsScreen.kt
@@ -1,6 +1,7 @@
 package com.example.jetbookreader.screens.details
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -23,8 +25,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.core.text.HtmlCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
@@ -102,7 +108,7 @@ fun ShowBookDetails(bookInfo: Resource<Item>, navController: NavController) {
 
     Text(
         text = bookData?.title.toString(),
-        style = MaterialTheme.typography.headlineMedium,
+        style = MaterialTheme.typography.headlineSmall,
         overflow = TextOverflow.Ellipsis,
         maxLines = 19
     )
@@ -110,13 +116,41 @@ fun ShowBookDetails(bookInfo: Resource<Item>, navController: NavController) {
     Text(text = "Page Count: ${bookData?.pageCount}")
     Text(
         text = "Categories: ${bookData?.categories}",
-        style = MaterialTheme.typography.labelLarge
+        style = MaterialTheme.typography.labelLarge,
+        maxLines = 3,
+        overflow = TextOverflow.Ellipsis
     )
     Text(
         text = "Published: ${bookData?.publishedDate}",
         style = MaterialTheme.typography.bodyMedium
     )
     Spacer(modifier = Modifier.height(5.dp))
+
+    val cleanDescription = HtmlCompat.fromHtml(
+        bookData!!.description,
+        HtmlCompat.FROM_HTML_MODE_LEGACY
+    ).toString()
+
+    val localDims = LocalContext.current.resources.displayMetrics
+
+    Surface(
+        modifier = Modifier
+            .height(localDims.heightPixels.dp.times(0.09f))
+            .padding(4.dp),
+        shape = RectangleShape,
+        border = BorderStroke(
+            1.dp,
+            Color.DarkGray
+        )
+    ) {
+        LazyColumn(modifier = Modifier.padding(3.dp)) {
+            item {
+                Text(text = cleanDescription)
+            }
+
+        }
+
+    }
 
 
 }
