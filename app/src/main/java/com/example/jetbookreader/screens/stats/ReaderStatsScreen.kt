c56dadc0262f712d2af392d0d901487ade11232f
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-10-14 22:10:34 +0530

    Finish Book Stats Screen

--- app/src/main/java/com/example/jetbookreader/screens/stats/ReaderStatsScreen.kt
+++ app/src/main/java/com/example/jetbookreader/screens/stats/ReaderStatsScreen.kt
@@ -1,11 +1,19 @@
 package com.example.jetbookreader.screens.stats
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -14,6 +22,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -21,9 +30,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
 import com.example.jetbookreader.components.ReaderAppBar
 import com.example.jetbookreader.model.MBook
 import com.example.jetbookreader.screens.home.HomeScreenViewmodel
@@ -116,9 +128,83 @@ fun ReaderStatsScreen(
 
                     }
                 }
+                if (viewModel.data.value.loading == true) {
+                    LinearProgressIndicator()
+                } else {
+                    Divider()
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(),
+                        contentPadding = PaddingValues(16.dp)
+                    ) {
+
+                        val readBooks: List<MBook> =
+                            if (!viewModel.data.value.data.isNullOrEmpty()) {
+                                viewModel.data.value.data!!.filter { mBook ->
+                                    (mBook.userId == currentUser?.uid) && (mBook.finishedReading != null)
+                                }
+                            }else{
+                                emptyList()
+                            }
+                        items(items=readBooks){book->
+                            BookRowStats(book = book)
+
+
+                        }
+                    }
+                }
+
             }
+        }
+    }
+}
+@Composable
+fun BookRowStats(book: MBook) {
+    Card(modifier = Modifier
+        .clickable {
 
-
+        }
+        .fillMaxWidth()
+        .height(110.dp)
+        .padding(8.dp),
+        shape = RectangleShape,
+        elevation = CardDefaults.cardElevation(7.dp)) {
+        Row(
+            modifier = Modifier.padding(5.dp),
+            verticalAlignment = Alignment.Top
+        ) {
+            val imageUrl: String = if (book.photoUrl.toString().isEmpty()) {
+                "http://books.google.com/books/content?id=zmnhDAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            } else {
+                book.photoUrl.toString()
+            }
+            Image(
+                painter = rememberAsyncImagePainter(model = imageUrl),
+                contentDescription = "book image",
+                modifier = Modifier
+                    .width(80.dp)
+                    .fillMaxHeight()
+                    .padding(end = 8.dp)
+            )
+            Column {
+                Text(text = book.title.toString(), overflow = TextOverflow.Ellipsis)
+                Text(
+                    text = "Author:${book.authors}",
+                    overflow = TextOverflow.Clip,
+                    style = MaterialTheme.typography.labelMedium
+                )
+                Text(
+                    text = "Published Date:${book.publishedDate}",
+                    overflow = TextOverflow.Clip,
+                    style = MaterialTheme.typography.labelMedium
+                )
+                Text(
+                    text = "${book.categories}",
+                    overflow = TextOverflow.Clip,
+                    style = MaterialTheme.typography.labelMedium
+                )
+            }
         }
     }
 }
